ab73402dcfea8b1a67f94daa7e159a0ac46d255a
kharyone <kharygill@gmail.com>
2021-03-29 21:32:15 -0400

    assignment5

---
+++ Assignment 4/js/script.js
@@ -0,0 +1,16 @@
+yaakovGreeter.sayHello();
+johnGreeter.sayGoodBye();
+jenGreeter.sayGoodBye();
+jasonGreeter.sayGoodBye();
+paulGreeter.sayHello();
+frankGreeter.sayHello();
+larryGreeter.sayHello();
+paulaGreeter.sayHello();
+lauraGreeter.sayHello();
+jimGreeter.sayGoodBye();
+
+
+
+
+
+
